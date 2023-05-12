d6993739b84fa2b76f534334df4cb6dc98e39aad
henrique <henriquelc@dcc.ufrj.br>
2023-05-12 11:24:49 -0300

    questao11

--- chapter_3/hash.cpp
+++ chapter_3/hash.cpp
@@ -4,23 +4,80 @@
 //division method
 //find prime close to load factor
 
-size_t division_method(size_t k, int m){
-    return k%m;
+struct fruta{
+    int chave;
+    int coisa;
+    struct fruta *ant;
+    struct fruta *prox;
+};
+
+
+
+class hash{
+    private:
+        std::vector<struct fruta*> cups;
+        size_t a;
+        size_t b;
+
+    public:
+        hash(){
+            for(size_t i = 0; i < 31; i++){
+                cups.push_back(nullptr);
+            }
+
+            a = rand()%30 + 1;
+            b = rand()%31;
+        }
+
+        size_t hashing(int k){
+            return (a*k + b)%31;
+        }
+
+        void insert(fruta f){
+            size_t pos = hashing(f.chave);
+
+            if(cups.at(pos) == nullptr){
+                f.ant = nullptr;
+                f.prox = nullptr;
+                cups.at(pos) = new fruta(f);
+                return;
+            }
+
+            else{
+                fruta *ptr = cups.at(pos);
+                while(ptr->prox != nullptr){
+                    ptr = ptr->prox;
+                }
+                f.ant = ptr;
+                f.prox = nullptr;
+                ptr->prox = new fruta(f);
+            }
+            
+            return;
+        }
+};
+
+double square_root(double x,int terms){
+    double n = x;
+    for(int i = 0; i < terms;i++){
+        n = (n + x/n)/2;
+    }
+    return n;
 }
 
-size_t multiplication_method(double A, int p, int k){
-    int w = 32;
-    size_t product =size_t(A*(1<<w));
-    std::cout << product << std::endl;
-    size_t last_32bits  = (k*product) % (1<<w);
-    std::cout << last_32bits << std::endl;
-    size_t p_bits = last_32bits>>w;
-    p_bits = p_bits>>(w-p);
-
-    return p_bits;
+int hash11(int m, int k){
+    double a = square_root(5,5);
+    double point = k*a - int(k*a);
+    return int(m*point);
 }
 
 int main(void){
-    std::cout << multiplication_method(0.618033,14, 123456) << std::endl;
+    std::cout << hash11(1000,61) << std::endl;
+    std::cout << hash11(1000,62) << std::endl;
+    std::cout << hash11(1000,63) << std::endl;
+    std::cout << hash11(1000,64) << std::endl;
+    std::cout << hash11(1000,65) << std::endl;
+
+    // std::cout<< square_root(5,5)<<std::endl;
     return 0;
 }
