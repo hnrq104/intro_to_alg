ee4034fa951c5231bf6e93244cda9631bde95a5d
hrq <henriquelc@dcc.ufrj.br>
2023-08-31 10:06:53 -0300

    done

--- part_6/all_pairs.cpp
+++ part_6/all_pairs.cpp
@@ -26,12 +26,27 @@ vector<vector<double>> extend_shortest_paths(vector<vector<double>> &L, vector<v
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             for(int k = 0; k < n; k++){
-                Lnew.at(i).at(j) = min(Lnew.at(i).at(j), L.at(i).at(k) + W.at(i).at(k));
+
+                Lnew.at(i).at(j) = min(Lnew.at(i).at(j), L.at(i).at(k) + W.at(k).at(j));
+                /*if Lnew changes -> P.at(i).at(j) = k*/
             }
         }
     }
 
     return Lnew;
+}
+
+template<typename T>
+void print_matrix(vector<vector<T>> L){
+    for(vector<T> v : L){
+        for(T d : v){
+            if(d == DBL_MAX){
+                cout << "infty" << " ";
+            }
+            else cout << d << " ";
+        }
+        cout << endl;
+    } 
 }
 
 vector<vector<double>> slow_all_pairs_shortest_paths(vector<vector<double>> W){
@@ -40,6 +55,14 @@ vector<vector<double>> slow_all_pairs_shortest_paths(vector<vector<double>> W){
 
     for(int m = 2; m < n; m++){
         L = extend_shortest_paths(L,W);
+        
+        /*printa a matriz*/
+        /*
+        cout << "L" << m << " = " << endl;
+        print_matrix(L);
+        cout << endl;
+        */
+
     }
 
     return L;
@@ -52,10 +75,21 @@ vector<vector<double>> faster_all_pairs_shortest_path(vector<vector<double>> W){
     while(m < n-1){
         L = extend_shortest_paths(L,L);
         m *= 2;
+
+        /*
+        cout << "L" << m << " = " << endl;
+        print_matrix(L);
+        cout << endl;
+        */
     }
 
     return L;
 }
+
+/*25.1-9 check if diagonals changed*/
+
+/*25.1-10 first time diagonals changed, go back in cycle*/
+
 
 
 int main(void){
@@ -63,13 +97,35 @@ int main(void){
     int n = 5;
     vector<vector<double>> Lnew(n,vector<double>(n,DBL_MAX)); /*initialize L' with all values at infinity*/
 
-    for(vector<double> v : Lnew){
+    vector<vector<double>> W = {
+        {0,DBL_MAX,DBL_MAX,DBL_MAX,-1,DBL_MAX},
+        {1,0,DBL_MAX,2,DBL_MAX,DBL_MAX},
+        {DBL_MAX,2,0,DBL_MAX,DBL_MAX,-8},
+        {-4,DBL_MAX,DBL_MAX,0,3,DBL_MAX},
+        {DBL_MAX,7,DBL_MAX,DBL_MAX,0,DBL_MAX},
+        {DBL_MAX,5,10,DBL_MAX,DBL_MAX,0}
+    };
+
+    cout << "ORIGINAL WEIGHT MATRIX : " << endl;
+    for(vector<double> v : W){
         for(double d : v){
             cout << d << " ";
         }
         cout << endl;
     }
+    cout << endl;
+
+    cout << "SLOW ALL PAIRS SHORTEST PATH ITERATIONS : " << endl; 
+    slow_all_pairs_shortest_paths(W);
+    cout<<endl;
+
+    cout << "FASTER ALL PAIRS SHORTEST PATH ITERATIONS : " << endl; 
+    vector<vector<double>> complete = faster_all_pairs_shortest_path(W);
+    print_matrix(complete);
+    cout<<endl;
     
+
+    cout << (-2.0 + -1.0 == -3.0) << endl; 
     return 0;
 
 
