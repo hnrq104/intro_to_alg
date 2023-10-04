f62c9b81f08d9e36d0e4ef88642134bf0bbc94b4
hrq <henriquelc@dcc.ufrj.br>
2023-10-03 23:16:44 -0300

    had got stuff wrong, but now its all good

--- part_7/chapter_30/polynomials.cpp
+++ part_7/chapter_30/polynomials.cpp
@@ -59,8 +59,8 @@ vector<complex<double>> division(vector<complex<double>> &lhs, complex<double> x
     return result;
 }
 
-/*recursive fft, I will suppose the vector given has size 2^n*/
-vector<complex<double>> recursive_fft(vector<complex<double>> &a){
+/*recursive dft, I will suppose the vector given has size 2^n*/
+vector<complex<double>> recursive_dft(vector<complex<double>> &a){
     uint n = a.size();
     if(n == 1){
         return {a.at(0)}; // a avaliated at w01 = 1
@@ -76,8 +76,8 @@ vector<complex<double>> recursive_fft(vector<complex<double>> &a){
         a1.at(i) = a.at(2*i + 1); 
     }
 
-    auto y0 = recursive_fft(a0);
-    auto y1 = recursive_fft(a1);
+    auto y0 = recursive_dft(a0);
+    auto y1 = recursive_dft(a1);
 
     vector<complex<double>> y(n);
     //A(x) = A[0](x^2) + x*A[1](x^2)
@@ -93,15 +93,14 @@ vector<complex<double>> recursive_fft(vector<complex<double>> &a){
 }
 
 
-/*recursive fft, I will suppose the vector given has size 2^n*/
-vector<complex<double>> recursive_inverse_dft(vector<complex<double>> &a){
+vector<complex<double>> recursive_inverse_dft_aux(vector<complex<double>> &a){
     uint n = a.size();
     if(n == 1){
         return {a.at(0)}; // a avaliated at w01 = 1
     }
 
     complex<double> wn = {cos(2*M_PI/n), sin(2*M_PI/n)}; //principal nth root of unity
-    wn = 1./wn;// need wn^-1
+    wn = 1./wn;
     complex<double> w = 1; 
 
     vector<complex<double>> a0(n/2,0); //coefficients of even powers
@@ -111,91 +110,214 @@ vector<complex<double>> recursive_inverse_dft(vector<complex<double>> &a){
         a1.at(i) = a.at(2*i + 1); 
     }
 
-    auto y0 = recursive_fft(a0);
-    auto y1 = recursive_fft(a1);
+    auto y0 = recursive_inverse_dft_aux(a0);
+    auto y1 = recursive_inverse_dft_aux(a1);
 
     vector<complex<double>> y(n);
-    double len = n;//divide by n
     //A(x) = A[0](x^2) + x*A[1](x^2)
     for(uint k = 0; k < n/2; k++){
         //yk = y0_k + w^{k}_n * y1_k for every k
         auto tmp = w*y1.at(k); //complex tmp
-        y.at(k) = (y0.at(k) + tmp)/len;
-        y.at(k + n/2) = (y0.at(k) - tmp)/len;
+        y.at(k) = y0.at(k) + tmp;
+        y.at(k + n/2) = y0.at(k) - tmp;
         w = w*wn;
     }
 
     return y;
 }
 
-// vector<complex<double>> iterative_fft(vector<complex<double>> &a){
-//     //bit reverse copy a,A
-//     vector<complex<double>> A;
-//     uint n = a.size();
-//     for(uint m = 1; m < n; m*=2){
+/*recursive dft, I will suppose the vector given has size 2^n*/
+vector<complex<double>> recursive_inverse_dft(vector<complex<double>> &a){
+    auto y = recursive_inverse_dft_aux(a);
+    double n = a.size();
+    for(auto &i : y){
+        i/=n;
+    }
+    return y;
+}
+
+uint bit_reversal(uint a,uint size){
+    uint rev = 0;
+    while(size > 1){ /*we're seing a power of two, so the last one shouldn't count*/
+        rev = rev << 1;
+        if(a&1){
+            rev = rev^1; /*this xor will add the last bit as one*/
+        }
+        a = a >> 1;
+        size = size >> 1;
+    }
+    
+    return rev;
+}
+
+vector<complex<double>> iterative_dft(vector<complex<double>> &a){
+    //bit reverse copy a,A
+    vector<complex<double>> A(a.size());
+    uint n = a.size();
+
+    /*bit reverse A from a*/
+    for(uint i = 0; i < n; i++){
+        A.at(bit_reversal(i,n)) = a.at(i);
+    }
+    
+    for(uint m = 1; m <= n; m*=2){
+        complex<double> wn = {cos(2*M_PI/m), sin(2*M_PI/m)};  /*i think there's a smarter way*/
         
-//     // complex<double> wn = {cos(2*M_PI/n), sin(2*M_PI/n)}; 
-//         for(uint k = 0; k < n; k+=m){
-//             complex<double> w = 1;
+        complex<double> w = 1; /*this change came with 30.3-3 it is really better*/
+        //i had thought of some optimization, glady it came as a question
+        for(uint j = 0; j < m/2; j++){
+ 
+            for(uint k = 0; k < n; k+=m){
+                auto t = w*A.at(k + j + m/2);
+                auto u = A.at(k+j);
 
-//             for(uint j = 0; j < m/2; j++){
-//                 auto t = w*A.at(k + j + m/2);
-//                 auto u = A.at(k+j);
+                A.at(k+j) = u + t;
+                A.at(k+j+ m/2) = u - t;
+            }
+            
+            w *= wn;
+        }
+    }
 
-//                 A.at(k+j) = u - t;
-//                 A.at(k+j+ m/2)
+    return A;
+}
 
 
-//             }
-//         }
+vector<complex<double>> iterative_inverse_dft(vector<complex<double>> &a){
+    uint n = a.size();
+    vector<complex<double>> A(n);
+    //bit reverse copy a,A
+    for(uint i = 0; i < n; i++){
+        A.at(bit_reversal(i,n)) = a.at(i);
+    }
+    
+    for(uint m = 1; m <= n; m*=2){
+        complex<double> wn = complex<double>({cos(2*M_PI/m), sin(2*M_PI/m)});  /*i think there's a smarter way*/
+        wn = 1./wn;
 
-//     }
+        complex<double> w = 1;
+        for(uint j = 0; j < m/2; j++){
+        
+            for(uint k = 0; k < n; k+=m){
+                auto t = w*A.at(k + j + m/2);
+                auto u = A.at(k+j);
 
-// }
+                A.at(k+j) = (u + t);
+                A.at(k+j+ m/2) = (u - t);
+            }
+            
+            w *= wn;
+        }
+    }
 
+    /*bit reverse A*/
+    double tam = n;
+    for(auto &i : A){
+        i /= tam;
+    }
+
+    return A;
+}
+
+/*I really don't know if I should, nor that I have the mental capacity
+to do this right now, but i will try
+
+Update:: i had, it worked lol
+30.3-2*/
+
+/*upside down inverse dft, should yield a normal dft with reverse bits at bottom*/
+vector<complex<double>> dft_upside_down(vector<complex<double>> &a){ 
+    uint n = a.size();
+    vector<complex<double>> A(n);
+    //bit reverse copy a,A
+    
+    double tam = n;
+    for(auto &i : A){
+        i *= tam;
+    }
+        
+    for(uint m = n; m > 0; m/=2){
+        complex<double> wn = complex<double>({cos(2*M_PI/m), sin(2*M_PI/m)});  /*i think there's a smarter way*/
+        wn = 1./wn;
+
+        for(uint k = 0; k < n; k+=m){
+            complex<double> w = 1;
+            for(uint j = 0; j < m/2; j++){
+                // auto t = w*A.at(k + j + m/2);
+                // auto u = A.at(k+j);
+
+                // A.at(k+j) = (u + t);
+                // A.at(k+j+ m/2) = (u - t);
+                auto u = (A.at(k + j) + A.at(k + j + m/2))/2.0; //u
+                auto t = (A.at(k + j) - A.at(k + j + m/2))/(2.*w); //t
+                A.at(k+j) = u;
+                A.at(k+j+m/2) = t;
+
+                w *= wn;
+            }
+        }
+    }
+
+    /*bit reverse A*/
+    for(uint i = 0; i < n; i++){
+        A.at(bit_reversal(i,n)) = a.at(i);
+    }
+
+    return A;
+}
 
 
 
 int main(void){
-    vector<complex<double>> divided = {{3,0},{-4,0},{1,0}};
-    
-    auto result = division(divided,{2,0});
-    for(auto i : result){
-        cout << i << " ";
-    }
-    cout << endl;
 
     /*exercise 30.2-2*/
-    vector<complex<double>> t = {{0,0},{1,0},{2,0},{3,0}};
-    auto dft = recursive_fft(t);
+    cout << "recursive dft: "; 
+    vector<complex<double>> t = {{1,0},{2,0},{3,0},{4,0}};
+    auto dft = recursive_dft(t);
     for(auto i : dft){
         cout << i << " ";
     }
     cout << endl;
 
-    /*test inverse */
-    auto inverse = recursive_inverse_dft(dft);
-    for(auto i : inverse){
+    /*text iterative*/
+    cout << "iterative dft: "; 
+    auto iterative = iterative_dft(t);
+    for(auto i : iterative){
         cout << i << " ";
     }
     cout << endl;
 
-    vector<complex<double>> A = {{1,0},{2,0},{3,0},{4,0}};
-    vector<complex<double>> B = {{0,0},{2,0},{4,0},{8,0}};
-
-    auto dfa = recursive_fft(A);
-    auto dfb = recursive_fft(B);
-
-    vector<complex<double>> product(A.size());
-    for(uint i = 0; i < A.size(); i++){
-        product.at(i) = A.at(i)*B.at(i);
-    }
-
-    auto reverse = recursive_inverse_dft(product);
+    cout << "recursive inverse dft: "; 
+    auto reverse = recursive_inverse_dft(dft);
     for(auto i : reverse){
         cout << i << " ";
     }
     cout << endl;
 
+
+    cout << "iterative inverse dft: "; 
+    auto reverse_it = iterative_inverse_dft(iterative);
+    for(auto i : reverse_it){
+        cout << i << " ";
+    }
+    cout << endl;
+
+
+    cout << "true inverse inverse dft: "; /*FUNCIONOU KKKKKKKKKKKS*/
+    auto up = dft_upside_down(iterative);
+    for(auto i : up){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    //exercise 30.3.1
+    vector<complex<double>> e = {{0,0},{2,0},{3,0},{-1,0},{4,0},{5,0},{7,0},{9,0}};    
+    auto it = iterative_dft(e);
+    for(auto i : it){
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    
     return 0;
 }
